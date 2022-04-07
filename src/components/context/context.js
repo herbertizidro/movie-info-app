1d4ba78a1ef0711a0a9dde87788ffe2dfd6bce6e
Herbert Izidro <herbertsouza80@yahoo.com>
2022-04-07 03:33:05 -0300

    comentario adicionado

--- src/components/context/context.js
+++ src/components/context/context.js
@@ -5,6 +5,19 @@ import React from 'react'
 const AppContext = React.createContext()
 
 export const AppProvider = AppContext.Provider;
+/* 
 export const AppConsumer = AppContext.Consumer;
+  
+  //caso fosse acessar o contexto dessa forma: 
+  return(
+    <AppConsumer.Consumer>
+      {value => (
+        <>
+          <h3>{value.title}</h3>
+        </>
+       )}
+    </AppConsumer.Consumer>
+   )
+*/
 
 export default AppContext;
